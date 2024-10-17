ad820cf02956e021daaa186adf029867f300fafa
Thi-Huynh01 <huynhthi265@gmail.com>
2024-10-17 00:39:20 -0500

    changed filereader.java to have all the data stored in it.

--- src/FileReader.java
+++ src/FileReader.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class FileReader {
 
-    public static ArrayList<DataSet> readFile (String fileName) throws IOException {
+    public ArrayList<DataSet> readFile (String fileName) throws IOException {
         String contents = Files.readString(Path.of(fileName), StandardCharsets.UTF_8);
         Scanner sc = new Scanner(contents);
         ArrayList<DataSet> dataSets = new ArrayList<>();
@@ -18,7 +18,47 @@ public class FileReader {
             DataSet dataset = new DataSet(data[0], data[1], data[2], data[3], data[4]);
             dataSets.add(dataset);
         }
+
+        sc.close();
+
         return dataSets;
     }
 
+    // Will return a long array in the following format:
+    // {count of white people, count of asian people, count of hispanic people, count of black people, count of males, count of females}
+
+    public long [] getData (String ethnicityFile, String genderFile) throws IOException {
+        ArrayList<DataSet> DataEth = readFile(ethnicityFile),
+                DataGen = readFile(genderFile);
+
+        return new long[]{
+                DataEth.stream()
+                        .filter(s -> s.ethnicity_or_gender().equals("white"))
+                        .count(),
+                DataEth.stream()
+                        .filter(s -> s.ethnicity_or_gender().equals("asian"))
+                        .count(),
+                DataEth.stream()
+                        .filter(s -> s.ethnicity_or_gender().equals("hispanic"))
+                        .count(),
+                DataEth.stream()
+                        .filter(s -> s.ethnicity_or_gender().equals("black"))
+                        .count(),
+                DataGen.stream()
+                        .filter(s -> s.ethnicity_or_gender().equals("male"))
+                        .count(),
+                DataGen.stream()
+                        .filter(s -> s.ethnicity_or_gender().equals("female"))
+                        .count()
+        };
+    }
+
+    public long[] getCCData () throws IOException {
+        return getData("src\\cc_ethnic_final.csv", "src\\cc_gender_final.csv");
+
+    }
+
+    public long[] getSBData () throws IOException {
+        return getData("src\\sb_ethnic_final.csv", "src\\sb_gender_final.csv");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Objects;
 
 // The CSV files are downloaded from this link
@@ -8,38 +9,17 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        // Ethnicity Data Sets
-        ArrayList<DataSet> cc_ethnicity = FileReader.readFile("src\\cc_ethnic_final.csv");
-        ArrayList<DataSet> sb_ethnicity = FileReader.readFile("src\\sb_ethnic_final.csv");
-
-        // Gender Data Sets
-        ArrayList<DataSet>cc_gender = FileReader.readFile("src\\cc_gender_final.csv");
-        ArrayList<DataSet>sb_gender = FileReader.readFile("src\\sb_gender_final.csv");
-
-        long [] cc_eth_count = getEthnicityCount(cc_ethnicity);
-        long [] sb_eth_count = getEthnicityCount(sb_ethnicity);
+        FileReader f = new FileReader();
+        for (long num : f.getCCData()) {
+            System.out.println(num);
+        }
+        System.out.println();
+        for (long sb : f.getSBData()) {
+            System.out.println(sb);
+        }
 
     }
 
-    public static long[] getEthnicityCount (ArrayList<DataSet> ethnicity_file) {
 
-
-        long white_count = 0, asian_count = 0, hispanic_count = 0, black_count = 0;
-
-         white_count = ethnicity_file.stream()
-                .filter(s -> s.ethnicity_or_gender().equals("white"))
-                .count();
-         asian_count = ethnicity_file.stream()
-                .filter(s -> s.ethnicity_or_gender().equals("asian"))
-                .count();
-         hispanic_count = ethnicity_file.stream()
-                .filter(s -> s.ethnicity_or_gender().equals("hispanic"))
-                .count();
-         black_count = ethnicity_file.stream()
-                 .filter(s -> s.ethnicity_or_gender().equals("black"))
-                 .count();
-
-        return new long[]{white_count, asian_count, hispanic_count, black_count};
-    }
 
 }
